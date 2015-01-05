36863ecd078f8a532ec657817d0b0ea992be5317
Marshall Thompson <marshall@creativeideal.net>
2015-01-05 09:23:25 -0700

    Documented attr.

--- mongoServer.js
+++ mongoServer.js
@@ -1,16 +1,26 @@
-var params = {
+var attrs = {
 	'client':null,
 	'db':null,
 	'admin':null
 };
 
-exports.attr = function(param, variable){
-	if (param) {
-		if (variable) {
-			params[param] = variable;
-			return variable;
+/**
+ * attr Getter / Setter
+ *
+ * Get data by passing in an attrName.  Set data by passing in
+ * the data as a second argument.
+ *
+ * @param  {String} attrName    Required attribute name.
+ * @param  {[type]} data Used when setting data to an attrName.
+ * @return Whatever data is stored in memory for the attrName.
+ */
+exports.attr = function(attrName, data){
+	if (attrName) {
+		if (data) {
+			attrs[attrName] = data;
+			return data;
 		}
-		return params[param];
+		return attrs[attrName];
 	} else {
 		new Error('Must pass in Param');
 	}
