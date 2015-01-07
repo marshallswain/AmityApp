9ea0e0595c0ccc035bd855d9dc8a14309ca5ece6
Marshall Thompson <marshall@creativeideal.net>
2015-01-07 13:35:12 -0700

    Basic syntax for setting up Amity.

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
+'use strict';
+
 var feathers = require('feathers'),
-  bodyParser = require('body-parser')
+  mongo = require('mongoskin'),
+	feathersMongo = require('feathers-mongodb'),
+  bodyParser = require('body-parser'),
   app = feathers();
-  app.use(bodyParser.json());
-  app.use(feathers.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(feathers.static(__dirname + '/public'));
 
+var AmityMongoDB = require('amity-mongodb'),
+  config = require('./config');
 
-var amity = require('amity'),
-  AmityMongoDB = require('amity-mongodb'),
-  mConfig = require('./config');
+var connectionString = 'mongodb://localhost:27017/amity',
+  ackOptions = {w: 1, journal: false, fsync: false, safe: false };
+var db = mongo.db(connectionString, ackOptions);
 
-var localMongo = new AmityMongoDB('host', config, app);
-amity.register(localMongo);
+var amityConfig = {
+	'store':{
+		'servers':feathersMongo({db:db, collection:'servers'}),
+		'users':feathersMongo({db:db, collection:'users'})
+	}
+};
+var amity = require('amity')(amityConfig, app);
+// amity.register(new AmityMongoDB('host', config, app));
 
 // Start the server.
 app.listen(config.site.port, function() {
