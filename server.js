5791f693364f31b74f4f86183395b97f5c2e7730
Marshall Thompson <marshall@creativeideal.net>
2015-01-20 08:39:19 -0700

    Can now get collection names from databases.

--- server.js
+++ server.js
@@ -22,9 +22,6 @@ var config = {
 // Start Amity with an adapter as a configuration store.
 amity.start(app, config, amity.mongodb('mongodb://localhost:27017/amity'));
 
-// app.use('/api/todos', require('./services/todos'));
-
-
 // Start the server.
 var port = 8081;
 app.listen(port, function() {
