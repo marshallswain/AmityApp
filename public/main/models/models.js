4311d67e20d58ffd25c5535dcd911949460a63ed
Marshall Thompson <marshall@creativeideal.net>
2015-01-24 13:22:45 -0700

    Made separate dynamic DB models storage.

--- public/main/models/models.js
+++ public/main/models/models.js
@@ -27,6 +27,24 @@ export var Todo = can.Feathers.Model.extend('todos', {
 
 var models = {};
 
+export function getDbModel(resource){
+	if (!models[resource]) {
+		models[resource] = can.Feathers.Model.extend({
+			resource:resource
+		}, {
+			dbSize(){
+				var bytes = this.attr('stats.dataSize');
+				if(bytes == 0) return '0 Byte';
+				var k = 1024;
+				var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+				var i = Math.floor(Math.log(bytes) / Math.log(k));
+				return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+			}
+		});
+	}
+	return models[resource];
+}
+
 export function getModel(resource){
 	if (!models[resource]) {
 		models[resource] = can.Feathers.Model.extend({
