c0fb9154198daf990c64ac87bfeb8e2ef66943ed
Marshall Thompson <marshall@creativeideal.net>
2015-02-18 09:03:27 -0700

    Simple add-collection component.

--- public/main/components/page-database/add-collection/add-collection.js
+++ public/main/components/page-database/add-collection/add-collection.js
@@ -1,12 +1,30 @@
 'use strict';
 
-// import appState from '../../appState';
 import './add-collection.css!';
+import {getCollModel} from '../../../models/collModels';
 
 can.Component.extend({
 	tag: 'add-collection',
 	template: can.view('/main/components/page-database/add-collection/add-collection.stache'),
-	scope: {},
+	scope: {
+		newCollection:{
+			name:''
+		},
+		create:function(scope){
+			if (scope.name) {
+				var resource = '/api/' + can.route.attr('hostname') +
+					'/' + can.route.attr('database.db') + '/' + '_collections';
+				var Model = getCollModel(resource);
+				new Model({
+					id:scope.name,
+					name:scope.name
+				}).save(function(model){
+					console.log('saved');
+					console.log(model);
+				});
+			}
+		}
+	},
 	events: {},
 	helpers:{}
 });
