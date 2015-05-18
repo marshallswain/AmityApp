752f20381473c621ce891e36e394b0a975e103ce
Marshall Thompson <marshall@creativeideal.net>
2015-05-17 18:19:08 -0600

    Removed not-needed files

--- routes/database.js
+++
@@ -1,7 +0,0 @@
-exports.viewDatabase = function(req, res) {
-  var ctx = {
-    title: 'Viewing Database: ' + req.dbName,
-    colls: req.collections[req.dbName]
-  };
-  res.render('database', ctx);
-}
